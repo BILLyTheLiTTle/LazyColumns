90c77ffc04f16ff8696a4c53939711611c41eb6c
v.tsapalos <v.tsapalos@kaizengaming.com>
2022-05-25 11:36:13 +0300

    Add examples

--- app/src/main/java/com/billythelittle/lazycolumnsexample/MainActivity.kt
+++ app/src/main/java/com/billythelittle/lazycolumnsexample/MainActivity.kt
@@ -1,35 +1,221 @@
 package com.billythelittle.lazycolumnsexample
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.annotation.RequiresApi
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.transform.CircleCropTransformation
+import com.billythelittle.lazycolumns.DoubleHeaderLazyColumn
+import com.billythelittle.lazycolumns.IndexedDataLazyColumn
+import com.billythelittle.lazycolumns.IndexedLazyColumn
 import com.billythelittle.lazycolumnsexample.ui.theme.LazyColumnsTheme
 
 class MainActivity : ComponentActivity() {
-
-
+    @OptIn(ExperimentalCoilApi::class, ExperimentalFoundationApi::class)
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LazyColumnsTheme {
                 // A surface container using the 'background' color from the theme
-                Index
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Surface(color = MaterialTheme.colors.background) {
+                    ExampleDoubleHeaderList(getTheData())
+//                    ExampleIndexedLazyColumn(getTheIndexedData())
+//                    ExampleIndexedDataLazyColumn(getTheIndexedData())
                 }
             }
         }
     }
+}
+
+@ExperimentalFoundationApi
+@RequiresApi(Build.VERSION_CODES.N)
+@Composable
+fun ExampleIndexedLazyColumn(data: List<CustomListItem2>,
+                             indices: List<Char> = ('A'..'Z').toList()) {
+    val lazyListState = rememberLazyListState()
+
+    IndexedLazyColumn(
+        // The list of the indices
+        indices = indices,
+        // The state of the main LazyColumn, the one with the real data
+        itemsListState = lazyListState,
+        // The modifier is exported for the Column, the one with the indices
+        modifier = Modifier
+            .background(color = Color.Transparent)
+            .height(300.dp),
+        // The way to connect the index with a data item (here the index item matches the first letter of the surname)
+        predicate = {
+            data.indexOfFirst { item ->
+                item.surname.startsWith(it.toString(), true)
+            }
+        },
+        // The list of the main data
+        lazyColumnContent = {
+            LazyColumn(state = lazyListState) {
+                items(data) {
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(5.dp)
+                            .clickable { },
+                        elevation = 10.dp
+                    ) {
+                        Column {
+                            Text(text = it.surname,
+                                fontSize = 17.sp, fontWeight = FontWeight.Bold, fontStyle = FontStyle.Italic,
+                                modifier = Modifier.padding(start = 10.dp))
+                            Text(text = it.name,
+                                fontSize = 15.sp, fontWeight = FontWeight.SemiBold,
+                                modifier = Modifier.padding(start = 15.dp))
+                        }
+                    }
+                }
+            }
+        },
+        // The item content for the indices list
+        indexedItemContent = { item, isSelected ->
+            Text(
+                modifier = Modifier
+                    .background(color = Color.Transparent),
+                color = if (isSelected) Color.Blue else Color.Black,
+                text = item.toString(),
+                fontSize = 20.sp)
+        }
+    )
+}
+
+@ExperimentalFoundationApi
+@RequiresApi(Build.VERSION_CODES.N)
+@Composable
+fun ExampleIndexedDataLazyColumn(data: List<CustomListItem2>,
+                                 indices: List<Char> = ('A'..'Z').toList()) {
+
+    IndexedDataLazyColumn(
+        // The list of the indices
+        indices = indices,
+        // The list of the actual data
+        data = data,
+        // The modifier is exported for the Column, the one with the indices
+        modifier = Modifier
+            .background(color = Color.Transparent)
+            .height(300.dp),
+        // The way to connect the index with a data item (here the index item matches the first letter of the surname)
+        predicate = {
+            data.indexOfFirst { item ->
+                item.surname.startsWith(it.toString(), true) }
+        },
+        // The list of the main data
+        mainItemContent = {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(5.dp)
+                    .clickable { },
+                elevation = 10.dp
+            ) {
+                Column {
+                    Text(text = data[it].surname,
+                        fontSize = 17.sp, fontWeight = FontWeight.Bold, fontStyle = FontStyle.Italic,
+                        modifier = Modifier.padding(start = 10.dp))
+                    Text(text = data[it].name,
+                        fontSize = 15.sp, fontWeight = FontWeight.SemiBold,
+                        modifier = Modifier.padding(start = 15.dp))
+                }
+            }
+        },
+        // The item content for the indices list
+        indexedItemContent = { item, isSelected ->
+            Text(
+                modifier = Modifier
+                    .background(color = Color.Transparent),
+                color = if (isSelected) Color.Blue else Color.Black,
+                text = item.toString(),
+                fontSize = 20.sp)
+        }
+    )
+}
+
+@OptIn(ExperimentalCoilApi:: class, ExperimentalAnimationApi::class)
+@ExperimentalFoundationApi
+@RequiresApi(Build.VERSION_CODES.N)
+@Composable
+fun ExampleDoubleHeaderList(data: List<CustomListItem>){
+    DoubleHeaderLazyColumn(data = data,
+        modifier = Modifier.fillMaxSize(),
+        headerContent = {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                elevation = 5.dp,
+                backgroundColor = Color.Red
+            ) {
+                Text(text = it, fontSize = 20.sp, modifier = Modifier.padding(5.dp))
+            }
+        },
+        subHeaderContent = {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                elevation = 5.dp,
+                backgroundColor = Color.Cyan
+            ) {
+                Text(text = it, fontSize = 17.sp, modifier = Modifier.padding(5.dp))
+            }
+        },
+        itemContent = {
+            val item = it as CustomListItem
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(5.dp)
+                    .clickable { },
+                elevation = 10.dp
+            ) {
+                Row() {
+                    Image(
+                        painter = rememberImagePainter(
+                            data = item.imageUrl,
+                            builder = {
+                                transformations(CircleCropTransformation())
+                            }
+                        ),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(80.dp)
+                            .padding(5.dp)
+                    )
+                    Column() {
+                        Text(text = item.title, fontSize = 15.sp)
+                        Text(text = item.description, fontSize = 10.sp)
+                    }
+                }
+            }
+        })
 }
 
 @Composable
---
+++ app/src/main/java/com/billythelittle/lazycolumnsexample/TheData.kt
@@ -0,0 +1,172 @@
+package com.billythelittle.lazycolumnsexample
+
+import com.billythelittle.lazycolumns.Item
+
+class CustomListItem(
+    override val type: String = "",
+    override val subType: String = "",
+    val title: String = "",
+    val description: String = "",
+    val imageUrl: String = ""
+): Item(type, subType)
+
+fun getTheData(): List<CustomListItem> {
+    return listOf(
+        CustomListItem("Movie", "Action",
+            "Shang-Chi and the Legend of the Ten Rings",
+            "Shang-Chi, the master of unarmed weaponry-based Kung Fu, is forced to " +
+                    "confront his past after being drawn into the Ten Rings organization.",
+            "https://threepixelslab.gr/wp-content/uploads/2021/04/Shang-Chi-and-the-Legend-of-the-Ten-Rings.jpg"),
+        CustomListItem("Movie", "Action",
+            "The Matrix Resurrections",
+            "The plot is currently unknown.",
+            "https://media.oneman.gr/onm-images/matrix-3.jpg"),
+        CustomListItem("Movie", "Action",
+            "Free Guy",
+            "A bank teller discovers that he's actually an NPC inside a brutal, open world video game.",
+            "https://www.athinorama.gr/lmnts/events/cinema/10072050/Poster.jpg"),
+        CustomListItem("Movie", "Action",
+            "The Suicide Squad",
+            "Supervillains Harley Quinn, Bloodsport, Peacemaker and a collection of nutty " +
+                    "cons at Belle Reve prison join the super-secret, super-shady Task Force X as they " +
+                    "are dropped off at the remote, enemy-infused island of Corto Maltese.",
+            "https://sm.ign.com/t/ign_gr/movie/s/suicide-sq/suicide-squad-2_9h82.200.jpg"),
+        CustomListItem("Movie", "Action",
+            "Kate",
+            "A female assassin has 24 hours to get vengeance on her murderer before she dies.",
+            "https://image.tmdb.org/t/p/w185/uQWgSRXeYRWCvGIX9LDNBW6XBYD.jpg"),
+        CustomListItem("Movie", "Horror",
+            "Candyman",
+            "A sequel to the horror film Candyman (1992) that returns to the now-gentrified " +
+                    "Chicago neighborhood where the legend began.",
+            "https://upreviews.net/images/artwork/upreviews_-KiisqOOM-GwNoLaCO_V.jpg"),
+        CustomListItem("Movie", "Horror",
+            "Don't Breathe 2",
+            "The sequel is set in the years following the initial deadly home invasion, " +
+                    "where Norman Nordstrom (Stephen Lang) lives in quiet solace until his past sins " +
+                    "catch up to him.",
+            "https://images-na.ssl-images-amazon.com/images/I/914Wg3bzCGL._RI_.jpg"),
+        CustomListItem("Movie", "Horror",
+            "Last Night in Soho",
+            "An aspiring fashion designer is mysteriously able to enter the 1960s where " +
+                    "she encounters a dazzling wannabe singer. But the glamour is not all it appears " +
+                    "to be and the dreams of the past start to crack and splinter into something darker.",
+            "https://deadline.com/wp-content/uploads/2021/06/last-night-in-soho-crop-excl-2.jpg"),
+        CustomListItem("Movie", "Horror",
+            "Malignant",
+            "Madison is paralyzed by shocking visions of grisly murders, and her torment " +
+                    "worsens as she discovers that these waking dreams are in fact terrifying realities.",
+            "https://media.oneman.gr/onm-images/HMKQGC5Q2NGSBCA3TIGZARMZFU.jpg"),
+        CustomListItem("Movie", "Animation",
+            "The Witcher: Nightmare of the Wolf",
+            "Escaping from poverty to become a witcher, Vesemir slays monsters for coin and " +
+                    "glory, but when a new menace rises, he must face the demons of his past.",
+            "https://upload.wikimedia.org/wikipedia/en/thumb/4/4d/The_Witcher_Nightmare_of_the_Wolf.jpg/220px-The_Witcher_Nightmare_of_the_Wolf.jpg"),
+        CustomListItem("Movie", "Animation",
+            "PAW Patrol: The Movie",
+            "Ryder and the pups are called to Adventure City to stop Mayor Humdinger " +
+                    "from turning the bustling metropolis into a state of chaos.",
+            "https://dx35vtwkllhj9.cloudfront.net/paramountpictures/paw-patrol-the-movie/images/regions/us/share-tout2.png"),
+        CustomListItem("Movie", "Musical",
+            "Cinderella",
+            "A modern movie musical with a bold take on the classic fairy tale. Our " +
+                    "ambitious heroine has big dreams and with the help of her fab Godmother, " +
+                    "she perseveres to make them come true.",
+            "https://lumiere-a.akamaihd.net/v1/images/g_cinderella1950_03_17805_4c9a7fe6.jpeg"),
+        CustomListItem("Book", "Horror",
+            "My Heart Is a Chainsaw",
+            "In her quickly gentrifying rural lake town Jade sees recent events only " +
+                    "her encyclopedic knowledge of horror films could have prepared her for",
+            "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1623264202l/55711617.jpg"),
+        CustomListItem("Book", "Horror",
+            "The Dead and the Dark",
+            "Courtney Gould’s thrilling debut The Dead and the Dark is about the things " +
+                    "that lurk in dark corners, the parts of you that can’t remain hidden, and about " +
+                    "finding home in places―and people―you didn’t expect.",
+            "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1599058814l/53141419.jpg"),
+        CustomListItem("Book", "Horror",
+            "Billy Summers",
+            "Billy Summers is a man in a room with a gun. He’s a killer for hire and the " +
+                    "best in the business.",
+            "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1618151020l/56852407.jpg"),
+        CustomListItem("Book", "Horror",
+            "A Lesson in Vengeance",
+            "Perched in the Catskill mountains, the centuries-old, ivy-covered campus was " +
+                    "home until the tragic death of her girlfriend.",
+            "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1605799295l/50999821.jpg"),
+        CustomListItem("Book", "Horror",
+            "Velvet Was the Night",
+            "From the New York Times bestselling author of Mexican Gothic comes a " +
+                    "“delicious, twisted treat for lovers of noir” about a daydreaming secretary, a " +
+                    "lonesome enforcer, and the mystery of a missing woman they’re both desperate to find.",
+            "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1617426360l/54746205.jpg"),
+        CustomListItem("Book", "Comic",
+            "Star Wars: War Of The Bounty Hunters - Boushh",
+            "THE SECRET ORIGIN OF BOUSHH! A “WAR OF THE BOUNTY HUNTERS” TIE-IN!",
+            "https://i.annihil.us/u/prod/marvel/i/mg/f/90/6142606a6160a/clean.jpg"),
+        CustomListItem("Book", "Comic",
+            "X-Men: The Trial of Magneto",
+            "Heroes of the Marvel Universe came to Krakoa for a memorial.",
+            "https://i.annihil.us/u/prod/marvel/i/mg/7/03/61426068ad880/clean.jpg"),
+        CustomListItem("Book", "Comic",
+            "Extreme Carnage: Agony",
+            "As the odds (and symbiotes!) stack against our heroes, is there any way " +
+                    "they can win against Carnage?",
+            "https://i.annihil.us/u/prod/marvel/i/mg/3/80/614260a6d95c8/clean.jpg"),
+        CustomListItem("Book", "Comic",
+            "The Last Annihilation: Wakanda",
+            "With the universe itself at stake, Black Panther enlists the might of the " +
+                    "Intergalactic Empire of Wakanda to help stop the dreaded Dormammu!",
+            "https://i.annihil.us/u/prod/marvel/i/mg/3/90/61426088ebb66/clean.jpg"),
+        CustomListItem("Book", "Comic",
+            "Black Widow",
+            "FRIEND OR FOE?",
+            "https://i.annihil.us/u/prod/marvel/i/mg/6/90/612e8e0826ce5/clean.jpg")
+    )
+}
+
+class CustomListItem2(
+    val surname: String = "",
+    val name: String = "",
+)
+
+fun getTheIndexedData(): List<CustomListItem2> {
+    return listOf(
+        CustomListItem2("A_Surname", "Name"),
+        CustomListItem2("A_Surname", "Name"),
+        CustomListItem2("B_Surname", "Name"),
+        CustomListItem2("B_Surname", "Name"),
+        CustomListItem2("B_Surname", "Name"),
+        CustomListItem2("B_Surname", "Name"),
+        CustomListItem2("B_Surname", "Name"),
+        CustomListItem2("B_Surname", "Name"),
+        CustomListItem2("B_Surname", "Name"),
+        CustomListItem2("B_Surname", "Name"),
+        CustomListItem2("C_Surname", "Name"),
+        CustomListItem2("C_Surname", "Name"),
+        CustomListItem2("C_Surname", "Name"),
+        CustomListItem2("G_Surname", "Name"),
+        CustomListItem2("G_Surname", "Name"),
+        CustomListItem2("G_Surname", "Name"),
+        CustomListItem2("G_Surname", "Name"),
+        CustomListItem2("G_Surname", "Name"),
+        CustomListItem2("G_Surname", "Name"),
+        CustomListItem2("G_Surname", "Name"),
+        CustomListItem2("G_Surname", "Name"),
+        CustomListItem2("G_Surname", "Name"),
+        CustomListItem2("I_Surname", "Name"),
+        CustomListItem2("I_Surname", "Name"),
+        CustomListItem2("I_Surname", "Name"),
+        CustomListItem2("K_Surname", "Name"),
+        CustomListItem2("K_Surname", "Name"),
+        CustomListItem2("K_Surname", "Name"),
+        CustomListItem2("K_Surname", "Name"),
+        CustomListItem2("K_Surname", "Name"),
+        CustomListItem2("K_Surname", "Name"),
+        CustomListItem2("K_Surname", "Name"),
+        CustomListItem2("K_Surname", "Name"),
+        CustomListItem2("K_Surname", "Name"),
+        CustomListItem2("Z_Surname", "Name"),
+        CustomListItem2("Z_Surname", "Name"),
+    )
+}
